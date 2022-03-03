fd96ed00ac8bccca2035c7e0161652d6d920c06f
lovnish044 <lovnishgarg044@gmail.com>
2022-03-03 21:52:11 +0530

    comp

---
+++ backend/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+
+
+const { generateFile } = require("./generateFile");
+
+const { addJobToQueue } = require("./jobQueue");
+const Job = require("./models/Job");
+
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/run", async (req, res) => {
+  const { language = "cpp", code } = req.body;
+
+  console.log(language, "Length:", code.length);
+
+  if (code === undefined) {
+    return res.status(400).json({ success: false, error: "Empty code body!" });
+  }
+  
+  const filepath = await generateFile(language, code);
+  
+  const job = await new Job({ language, filepath }).save();
+  const jobId = job["_id"];
+  addJobToQueue(jobId);
+  res.status(201).json({ jobId });
+});
+
+app.get("/status", async (req, res) => {
+  const jobId = req.query.id;
+
+  if (jobId === undefined) {
+    return res
+      .status(400)
+      .json({ success: false, error: "missing id query param" });
+  }
+
+  const job = await Job.findById(jobId);
+
+  if (job === undefined) {
+    return res.status(400).json({ success: false, error: "couldn't find job" });
+  }
+
+  return res.status(200).json({ success: true, job });
+});
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true ,useUnifiedTopology: true})
+  .then(() => app.listen(process.env.PORT, () => console.log(`Server Running on Port: http://localhost:${process.env.PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
